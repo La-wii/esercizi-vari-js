eb55ad057b55c8371e9351b6e9a26e145d77c10f
La-wii <sentilavale@gmail.com>
2021-05-30 12:34:57 +0200

    altri esercizi

--- js/script.js
+++ js/script.js
@@ -1,7 +1,64 @@
-
 // stampa i numeri da 1 a 10
-for (var i = 1; i <= 10; i++) {
-    console.log(i)
-}
+
+// for (var i = 1; i <= 10; i++) {
+
+//     console.log(i)
+
+// }
 
 
+// stampo i numeri da 5 a 50 multipli di 5
+
+// for (var i = 5; i <= 50; i++) {
+
+//     if (i % 5 == 0) {
+//         console.log(i);
+//     }
+
+// }
+
+
+// for (var i = 1; i <= 30; i++) {
+//     if ( i % 5 == 0 ) {
+//         console.log(i);
+//     }
+// }
+
+
+// for ( var i = 1; i <= 60; i++){
+//     if ( i % 6 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+// for (var i = 9; i<= 100; i++) {
+//     if (i % 9 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// for (var i = 4; i <= 80; i++) {
+//     if ( i % 4 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// var nomi = ["Giorgio", "Paolo", "Piero"]; 
+// console.log(nomi[0]);
+// console.log(nomi[1]);
+// console.log(nomi[2]);
+
+
+// var nomi = ["Eva", "Vale", "Ale"];
+// console.log(nomi[0]);
+// console.log(nomi[1]);
+// console.log(nomi[2]);
+
+
+// var nomi = ["Paolo", "Pippo", "Pluto"];
+// for (var i = 0; i < nomi.length; i++){
+//     console.log(nomi[i]);
+// }
